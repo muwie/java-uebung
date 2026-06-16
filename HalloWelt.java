bae772b1bc6fad98e922d70bfdab20a2aa24e0a6
muwie <muwieser@tsn.at>
2026-06-16 14:38:26 +0200

    Update print statement with a comment
    Added a comment to the print statement.

--- HalloWelt.java
+++ HalloWelt.java
@@ -1,6 +1,6 @@
 public class HalloWelt {
     public static void main(String[] args)
      {
-        System.out.println("Hallo GitHub!");
+        System.out.println("Hallo GitHub!"); //Diese Zeile habe ich auf GitHub bearbeitet
     }
 }
