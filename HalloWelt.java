3d13d89d645aa36462b0387a4c41de70406f19b9
muwie <muwieser@tsn.at>
2026-06-16 14:52:57 +0200

    Ausgabe Name & Datum

--- HalloWelt.java
+++ HalloWelt.java
@@ -2,5 +2,8 @@ public class HalloWelt {
     public static void main(String[] args)
      {
         System.out.println("Hallo GitHub!"); //Diese Zeile habe ich auf GitHub bearbeitet
+        System.out.println("Murielle Wieser");
+        System.out.println("16.06.2026");
+        
     }
 }
